27156c880fc3b66996cd0bcc6e1b31fd517f0253
risky-infinity <81591490+risky-infinity@users.noreply.github.com>
2024-02-18 14:29:39 +0530

    InvoiceDto Added

--- bookworm_middleware/src/main/java/com/bookworm/bookworm_middleware/dtos/InvoiceDto.java
+++ bookworm_middleware/src/main/java/com/bookworm/bookworm_middleware/dtos/InvoiceDto.java
@@ -1,5 +1,88 @@
 package com.bookworm.bookworm_middleware.dtos;
 
+import java.sql.Date;
+
 public class InvoiceDto {
+    private Date invoiceDate;
+    private Integer customerId;
+    private Float invoiceAmount;
+    private Integer quantity;
+    private Float basePrice;
+    private Float sellingPrice;
+    private Integer productId;
+    private String transactionTypeId;
+    private Integer rentingDays;
+
+    public Date getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(Date invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Float getInvoiceAmount() {
+        return invoiceAmount;
+    }
+
+    public void setInvoiceAmount(Float invoiceAmount) {
+        this.invoiceAmount = invoiceAmount;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Float getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(Float basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public Float getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Float sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getTransactionTypeId() {
+        return transactionTypeId;
+    }
+
+    public void setTransactionTypeId(String transactionTypeId) {
+        this.transactionTypeId = transactionTypeId;
+    }
+
+    public Integer getRentingDays() {
+        return rentingDays;
+    }
+
+    public void setRentingDays(Integer rentingDays) {
+        this.rentingDays = rentingDays;
+    }
 
 }
